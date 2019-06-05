16790b06734e4abeeb3b5671495129d04570dd89
Istvan Csaszar <icsaszar@outlook.com>
2019-06-05 16:04:17 +0300

    Adds rest client

--- code/frontend/src/rest/RestClient.ts
+++ code/frontend/src/rest/RestClient.ts
@@ -1,3 +1,9 @@
+import {Student, StudentGroup} from "../model/objects/user/Student";
+import {Teacher} from "../model/objects/user/Teacher";
+import {Admin} from "../model/objects/user/Admin";
+import {Activity} from "../model/objects/activity/Activity";
+import {ActivityEvent} from "../model/objects/activity/ActivityEvent";
+
 interface OkResponse {
     status: 'succeeded'
     data: Response
@@ -13,6 +19,8 @@ interface Failed {
 }
 
 export type ResponseData = OkResponse | ErrorResponse | Failed
+
+
 
 function onSuccess(response: Response): ResponseData {
     if (response.status === 200) {
@@ -137,11 +145,80 @@ export default class RestClient {
         return RestClient.makeAsyncRequest(info, init);
     }
 
+    static async logout(): Promise<ResponseData> {
+        if (this.client === undefined) throw Error("RestClient not initialized!");
+        let {info, init} = this.createPostRequest("logout",{});
+        this.client = undefined;
+        return RestClient.makeAsyncRequest(info, init);
+    }
+
     static async fetchDetails(): Promise<ResponseData> {
         if (this.client === undefined) throw Error("RestClient not initialized!");
         let {info, init} = this.createGetRequest("account");
         return RestClient.makeAsyncRequest(info, init);
     }
+
+    static async fetchStudentGroups(): Promise<ResponseData> {
+        if (this.client === undefined) throw Error("RestClient not initialized!");
+        let {info, init} = this.createGetRequest("data/groups");
+        return RestClient.makeAsyncRequest(info, init);
+    }
+
+    static async sendStudent(student: Student, password: string): Promise<ResponseData>{
+        if (this.client === undefined) throw Error("RestClient not initialized!");
+        let studentWithPassword = {
+            ...student,
+            password: password
+        };
+        let {info, init} = this.createPostRequest("admin/students", studentWithPassword);
+        return RestClient.makeAsyncRequest(info, init);
+    }
+
+    static async sendTeacher(teacher: Teacher, password: string): Promise<ResponseData>{
+        if (this.client === undefined) throw Error("RestClient not initialized!");
+        let teacherWithPassword = {
+            ...teacher,
+            password: password
+        };
+        let {info, init} = this.createPostRequest("admin/teachers", teacherWithPassword);
+        return RestClient.makeAsyncRequest(info, init);
+    }
+
+    static async sendAdmin(admin: Admin, password: string): Promise<ResponseData>{
+        if (this.client === undefined) throw Error("RestClient not initialized!");
+        let adminWithPassword = {
+            ...admin,
+            password: password
+        };
+        let {info, init} = this.createPostRequest("admin/students", adminWithPassword);
+        return RestClient.makeAsyncRequest(info, init);
+    }
+
+    static async sendStudentGroup(groupName: string): Promise<ResponseData>{
+        if (this.client === undefined) throw Error("RestClient not initialized!");
+        let group = new StudentGroup(groupName);
+        let {info, init} = this.createPostRequest("admin/groups", group);
+        return RestClient.makeAsyncRequest(info, init);
+    }
+
+    static async sendActivity(activity: Activity): Promise<ResponseData> {
+        if (this.client === undefined) throw Error("RestClient not initialized!");
+        let {init, info} = this.createPostRequest("admin/activities", activity);
+        return RestClient.makeAsyncRequest(info, init);
+    }
+
+    static async sendActivityEvent(activity: Activity, activityEvent: ActivityEvent): Promise<ResponseData> {
+        if (this.client === undefined) throw Error("RestClient not initialized!");
+        let {init, info} = this.createPostRequest(`admin/activities/${activity.id}/events`, activityEvent);
+        return RestClient.makeAsyncRequest(info, init);
+    }
+
+    static async fetchActivityData(): Promise<ResponseData>{
+        if (this.client === undefined) throw Error("RestClient not initialized!");
+        let {init, info} = this.createGetRequest("data/activity-data");
+        return RestClient.makeAsyncRequest(info, init);
+    }
+
 }
 
 
