efd7520cb932dfa829bd182bb11eda2b7d52e0af
Istvan Csaszar <icsaszar@outlook.com>
2019-06-06 18:33:18 +0300

    Renames copy-pasted functions

--- code/frontend/src/model/state/teachers/actions.ts
+++ code/frontend/src/model/state/teachers/actions.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
     ADD_NEW_TEACHER, AddNewTeacher,
     RECEIVE_TEACHERS,
@@ -8,57 +7,26 @@ import {
 } from "./types";
 import {Teacher} from "../../objects/user/Teacher";
 
-export function doReceiveStudentGroups(teachers: Teacher[]): ReceiveTeachersAction {
+export function doReceiveTeachers(teachers: Teacher[]): ReceiveTeachersAction {
     return {
         type: RECEIVE_TEACHERS,
         payload:{
             data: teachers
-=======
-import {StudentGroup} from "../../objects/user/Student";
-import {
-    ADD_NEW_STUDENT_GROUP,
-    AddNewStudentGroup,
-    RECEIVE_STUDENT_GROUPS,
-    ReceiveStudentGroupsAction,
-    REQUEST_STUDENT_GROUPS,
-    RequestStudentGroupsAction
-} from "./types";
-
-export function doReceiveStudentGroups(groups: StudentGroup[]): ReceiveStudentGroupsAction {
-    return {
-        type: RECEIVE_STUDENT_GROUPS,
-        payload:{
-            data: groups
->>>>>>> a7a15c76daf5bb2356740bf1305efe2e07ad72e6
         }
     }
 }
 
-<<<<<<< HEAD
-export function doRequestStudentGroups(): RequestTeachersAction {
+export function doRequestTeachers(): RequestTeachersAction {
     return {
         type: REQUEST_TEACHERS
     }
 }
 
-export function doAddStudentGroup(newTeacher: Teacher): AddNewTeacher {
+export function doAddTeacher(newTeacher: Teacher): AddNewTeacher {
     return {
         type: ADD_NEW_TEACHER,
         payload: {
             newTeacher: newTeacher
-=======
-export function doRequestStudentGroups(): RequestStudentGroupsAction {
-    return {
-        type: REQUEST_STUDENT_GROUPS
-    }
-}
-
-export function doAddStudentGroup(newGroup: StudentGroup): AddNewStudentGroup {
-    return {
-        type: ADD_NEW_STUDENT_GROUP,
-        payload: {
-            newGroup: newGroup
->>>>>>> a7a15c76daf5bb2356740bf1305efe2e07ad72e6
         }
     }
 }
--- code/frontend/src/model/state/teachers/reducers.ts
+++ code/frontend/src/model/state/teachers/reducers.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {ADD_NEW_TEACHER, RECEIVE_TEACHERS, REQUEST_TEACHERS, TeacherActions, TeacherState} from "./types";
 
 const initialState: TeacherState = {
@@ -9,31 +8,10 @@ const initialState: TeacherState = {
 export function teacherReducer(state: TeacherState = initialState, action: TeacherActions): TeacherState {
     switch (action.type) {
         case REQUEST_TEACHERS:
-=======
-import {
-    ADD_NEW_STUDENT_GROUP,
-    RECEIVE_STUDENT_GROUPS,
-    REQUEST_STUDENT_GROUPS,
-    StudentGroupActions,
-    StudentGroupState
-} from "./types";
-import {StudentGroup} from "../../objects/user/Student";
-
-
-const initialState: StudentGroupState = {
-    groups: [new StudentGroup("g1",1), new StudentGroup("g2", 2)],
-    isFetching: false
-};
-
-export function studentGroupReducer(state: StudentGroupState = initialState, action: StudentGroupActions): StudentGroupState {
-    switch (action.type) {
-        case REQUEST_STUDENT_GROUPS:
->>>>>>> a7a15c76daf5bb2356740bf1305efe2e07ad72e6
             return {
                 ...state,
                 isFetching: true
             };
-<<<<<<< HEAD
         case RECEIVE_TEACHERS:
             return {
                 ...state,
@@ -43,17 +21,6 @@ export function studentGroupReducer(state: StudentGroupState = initialState, act
             return {
                 ...state,
                 teachers: [...state.teachers, action.payload.newTeacher]
-=======
-        case RECEIVE_STUDENT_GROUPS:
-            return {
-                ...state,
-                groups: action.payload.data
-            };
-        case ADD_NEW_STUDENT_GROUP:
-            return {
-                ...state,
-                groups: [...state.groups, action.payload.newGroup]
->>>>>>> a7a15c76daf5bb2356740bf1305efe2e07ad72e6
             };
         default:
             return state;
